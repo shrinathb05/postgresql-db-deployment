d169fc8fd36362f46edc6b9a73b971f74d480b27
shree <shreebhosale05@gmail.com>
2026-08-25 18:03:02 +0000

    unnecessary files deleted

--- release/create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE emp (
-    emp_id      SERIAL PRIMARY KEY,
-    first_name  VARCHAR(50) NOT NULL,
-    last_name   VARCHAR(50) NOT NULL,
-    email       VARCHAR(100) UNIQUE NOT NULL,
-    department  VARCHAR(50),
-    salary      NUMERIC(10, 2),
-    hire_date   DATE DEFAULT CURRENT_DATE
-);
